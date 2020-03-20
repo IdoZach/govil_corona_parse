392e4190776f42148cb8eb2ca8ed73d94c2b83b0
ido <eedoza@gmail.com>
2020-03-20 20:12:29 +0200

    update

--- govil_corona_parser.py
+++ govil_corona_parser.py
@@ -21,11 +21,14 @@ class Parser(object):
             queries = (queries,)
         self.queries = queries
         self.addr_prefix = 'https://www.gov.il/he/departments/news/'
-        temp_dir = tempfile.mkdtemp()
-        self.tar_html_fname = osp.join(temp_dir,'govil_parse.html')
+
+        save_dir = './' #
+        s_queries = '_'.join(queries)
+        self.tar_fname = osp.join(save_dir,'govil_parse_{}_{}'.format(datetime.datetime.now().strftime('%d%m%y_%Hh'),s_queries))
+        print('target file name: {}'.format(self.tar_fname))
         self.res_data = None
     def get_addresses(self):
-        start_date = datetime.datetime(day=1,month=2,year=2020)
+        start_date = datetime.datetime(day=1,month=3,year=2020)
         end_date = datetime.datetime.now()
         delta = datetime.timedelta(days=1)
         cur = start_date
@@ -54,7 +57,7 @@ class Parser(object):
                     except Exception as e:
                         print(e)
                 if success:
-                    yield html
+                    yield html, url
                     #addresses.append(url)
                 if seek_next_page:
                     break
@@ -62,12 +65,10 @@ class Parser(object):
             cur += delta
         #return addresses
 
-    def parse_details(self,s=None):
-        example = "14.03.2020 שבת, 08:00 עד 11:00 בבית הכנסת 'אור וישועה', רח' טרומפלדור, חיפה (בנין בית חנניה למעלה)."
-        s = s or example
+    def parse_details(self,s=None,url=''):
         date = re.findall('\d\d\.\d\d\.\d\d\d\d',s)
         hours = re.findall('\d\d:\d\d',s)
-        return dict(date=date,hours=hours,all=s)
+        return dict(date=date,hours=hours,text=s,url=url)
 
     def read_page(self, html=None, addr='https://www.gov.il/he/departments/news/20032020_04'):
         if html is None:
@@ -77,25 +78,28 @@ class Parser(object):
         dat = []
         for line in text:
             if any([q in line for q in self.queries]):
-                di = self.parse_details(line)
-                print('date: {}, times: {}, all: {}'.format(di['date'], di['hours'], di['all']))
+                di = self.parse_details(line,addr)
+                print('date: {}, times: {}, all: {}'.format(di['date'], di['hours'], di['text']))
                 dat.append(di)
         return dat
     def read_pages(self):
         data = []
-        for html in self.get_addresses():
-            #print('parsing url {}'.format(url))
-            data.extend(self.read_page(html=html))
+        for html, url in self.get_addresses():
+            data.extend(self.read_page(html=html, addr=url))
         self.res_data = data
-    def show_parsed_data(self):
-        #data = pickle.load(open(self.cache_data, 'rb'))
+    def save_parsed_data(self):
         data = self.res_data
         df = pd.DataFrame(data)
-        df['all'] = df['all'].apply(str.strip)
-        open(self.tar_html_fname,'w').writelines(df.to_html())
-        print('written to {}'.format(self.tar_html_fname))
-        os.system('xdg-open {}'.format(self.tar_html_fname))
+        df['text'] = df['text'].apply(str.strip)
+        df['url'] = df['url'].apply(lambda x: '<a href={}>{}</a>'.format(x,x))
+        df = df.iloc[::-1] # reverse
+        open(self.tar_fname+'.html','w').writelines(df.to_html(escape=False))
+        open(self.tar_fname+'.md', 'w').writelines(df.to_markdown())
+        print('written HTML to {}'.format(self.tar_fname+'.html'))
+        print('written MD to {}'.format(self.tar_fname+'.md'))
+        os.system('xdg-open {}'.format(self.tar_fname+'.html'))
 if __name__ == '__main__':
     parser = Parser(queries='חיפה')
+    # parser = Parser(queries='תל אביב')
     parser.read_pages()
-    parser.show_parsed_data()
+    parser.save_parsed_data()
